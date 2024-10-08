eddf6805ed0dbd51ac46d7f66f755d493ed9c0fc
TheFatAndFurious <mrguerrilla@gmail.com>
2024-10-08 23:01:51 +0200

    initial commit

---
+++ src/main/java/BalancingAct.java
@@ -0,0 +1,7 @@
+public class BalancingAct {
+    public static void main (String[] var0){
+        System.out.println("test");
+        // Start a http server
+        // Create a handler that will forward http requests to the servers
+    }
+}
