67eefceb3ea3788d72fb56e8652d5385fdb88bd2
TheFatAndFurious <mrguerrilla@gmail.com>
2024-10-09 22:29:57 +0200

    server alive and listening

--- src/main/java/BalancingAct.java
+++ src/main/java/BalancingAct.java
@@ -1,6 +1,8 @@
 public class BalancingAct {
-    public static void main (String[] var0){
+    public static void main (String[] var0) throws Exception {
         System.out.println("test");
+        Server server = new Server();
+        server.Start(8079);
         // Start a http server
         // Create a handler that will forward http requests to the servers
     }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -3,27 +3,35 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.util.concurrent.Executor;
+
 public class Server {
 
 private HttpServer server;
 
-    public void Start(int Port)throws Exception{
-        server = HttpServer.create(new InetSocketAddress(Port), 0);
+    public void Start(int port) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
         server.createContext("/", new MyHandler());
-        server.setExecutor(null);
+        server.setExecutor((Executor) null);
         server.start();
+        System.out.println("Server started on port:" + port);
     }
 
     public void Stop(){
-        server.stop(0);
+           server.stop(0);
     }
 
     static class MyHandler implements HttpHandler{
 
         @Override
         public void handle(HttpExchange httpExchange) throws IOException {
-
+            String response = "This is a test";
+            httpExchange.sendResponseHeaders(200, response.length());
+            OutputStream os = httpExchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
         }
     }
 }
